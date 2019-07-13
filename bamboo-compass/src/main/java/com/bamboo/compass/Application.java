1681d6270e4594e7d6aa0130fd1e2f3f8c62c9d1
smile <smile.szx@outlook.com>
2019-07-13 14:33:16 +0800

    add rabbitmq test demo

---
+++ bamboo-compass/src/main/java/com/bamboo/compass/Application.java
@@ -0,0 +1,13 @@
+package com.bamboo.compass;
+
+/**
+ * @Description TODO
+ * @Author Thinkpad
+ * @Date 2019/7/4 8:25
+ **/
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("init a compass project");
+    }
+}
---
+++ bamboo-compass/src/main/java/com/bamboo/compass/common/Constant.java
@@ -0,0 +1,9 @@
+package com.bamboo.compass.common;
+
+/**
+ * @Description TODO
+ * @Author victor su
+ * @Date 2019/7/4 8:30
+ **/
+public class Constant {
+}
