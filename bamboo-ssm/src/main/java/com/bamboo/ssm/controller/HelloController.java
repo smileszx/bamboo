fd8064def02464040294517e7d23cf62c1ad0906
smile <smile.szx@outlook.com>
2019-07-18 20:43:33 +0800

    idea application context error

--- bamboo-ssm/src/main/java/com/bamboo/ssm/controller/HelloController.java
+++ bamboo-ssm/src/main/java/com/bamboo/ssm/controller/HelloController.java
@@ -1,11 +1,29 @@
 package com.bamboo.ssm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+
+	@ModelAttribute
+	@ResponseBody
+	public String init()
+	{
+		System.out.println("@ModelAttribute init");
+		return "@ModelAttribute init";
+	}
+
+	@ModelAttribute
+	@ResponseBody
+	public String start()
+	{
+		System.out.println("@ModelAttribute start");
+		return "@ModelAttribute start";
+	}
 
 	@RequestMapping("hello")
 	@ResponseBody
