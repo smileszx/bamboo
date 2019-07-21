6bd8ce9fb2a16fa09fbe5927e05d791ffaec975f
smile <smile.szx@outlook.com>
2019-07-21 23:24:59 +0800

    add mysql

---
+++ bamboo-distribute/src/main/java/com/bamboo/distribute/common/CommonTest.java
@@ -0,0 +1,16 @@
+package com.bamboo.distribute.common;
+
+import lombok.Data;
+import lombok.Value;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description TODO
+ * @Author victor su
+ * @Date 2019/7/19 22:49
+ **/
+@Data
+public class CommonTest {
+
+    private String name;
+}
