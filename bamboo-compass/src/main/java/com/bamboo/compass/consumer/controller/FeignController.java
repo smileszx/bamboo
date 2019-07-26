17fbde50c2e4c5b6d420ce243006410b13a1cd13
smile <smile.szx@outlook.com>
2019-07-26 22:53:20 +0800

    hystrix alarm

--- bamboo-compass/src/main/java/com/bamboo/compass/consumer/controller/FeignController.java
+++ bamboo-compass/src/main/java/com/bamboo/compass/consumer/controller/FeignController.java
@@ -3,9 +3,14 @@ package com.bamboo.compass.consumer.controller;
 import com.bamboo.compass.consumer.bean.Depart;
 import com.bamboo.compass.consumer.service.DepartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 @RequestMapping("/consumer/feign")
 @RestController
@@ -14,6 +19,11 @@ public class FeignController {
     //此处调用的是Feign Client，而非真正的业务接口
     @Autowired
     private DepartService departService;
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    private ForkJoinPool pool = new ForkJoinPool(5);
 
     @PostMapping("/save")
     public boolean saveHandle(@RequestBody Depart depart) {
@@ -31,7 +41,9 @@ public class FeignController {
     }
 
     @GetMapping("/get/{id}")
-    public Depart getHandle(@PathVariable("id") int id) {
+    public Depart getHandle(@PathVariable("id") int id, HttpServletRequest request) {
+        String key = request.getLocalAddr() + "_getDepartById_fallback";
+        queryCache(key);
         return departService.getDepartById(id);
     }
 
@@ -48,4 +60,31 @@ public class FeignController {
     public String portHandle() {
         return departService.portHandle();
     }
+
+    private void queryCache(String key) {
+        BoundValueOperations<String, String> ops = template.boundValueOps(key);
+        String value = ops.get();
+        if (value == null) {
+            synchronized (this) {
+                value = ops.get();
+                if (value == null) {
+                    sendFallBackMsg(key);
+                    ops.set("短信已发出", 10, TimeUnit.SECONDS);
+                }
+            }
+
+        }
+    }
+
+    private void sendFallBackMsg(String key) {
+        pool.submit(() ->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("发送服务异常报警短信： " + key);
+        });
+    }
+
 }
