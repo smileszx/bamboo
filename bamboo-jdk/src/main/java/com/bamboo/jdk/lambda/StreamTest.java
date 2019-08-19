68558be0aab705284fa7e0c8deaa6daf2cd89bf2
smile <smile.szx@outlook.com>
2019-08-19 23:25:41 +0800

    stream test

--- bamboo-jdk/src/main/java/com/bamboo/jdk/lambda/StreamTest.java
+++ bamboo-jdk/src/main/java/com/bamboo/jdk/lambda/StreamTest.java
@@ -1,8 +1,16 @@
 package com.bamboo.jdk.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @Description TODO
@@ -26,11 +34,56 @@ public class StreamTest {
             list.add(artist);
         }
 
-//        testForCycle(list);
-//        testLambdaStream(list);
-        testStream(list);
+        testReduce ();
     }
 
+    public static void testReduce () {
+        int count = Stream.of(10, 20, 30)
+                .reduce(0, new BinaryOperator<Integer>() {
+                    @Override
+                    public Integer apply(Integer acc, Integer element) {
+                        return acc + element;
+                    }
+                });
+
+        System.out.println(count);
+
+       // 0, (acc, element) -> acc + element
+    }
+
+    /**
+     * flatMap 对应Function函数式接口
+     */
+    public static void testFlatMap() {
+        List<Integer> together = Stream.of(Arrays.asList(12, 2), Arrays.asList(13, 4))
+                .flatMap(numbers -> numbers.stream())
+                .sorted()
+                .collect(toList());
+        System.out.println(together);
+    }
+
+    /**
+     * map 对应Function函数式接口
+     */
+    public static void testMap () {
+        List<String> collected = Stream.of("a", "b", "hello")
+                .map(new Function<String, String>() {
+                    @Override
+                    public String apply(String s) {
+                        return s.toUpperCase();
+                    }
+                })
+                .collect(toList());
+        System.out.println(collected);
+        //string -> string.toUpperCase()
+    }
+
+    public static void testCollect() {
+        List<String> collected = Stream.of("a", "b", "c")
+                .collect(toList());
+        boolean result = Arrays.asList("a", "b", "c").equals(collected);
+        System.out.println(result);
+    }
 
     public static void testStream (List<Artist> list) {
         list.stream().filter(artist -> {
@@ -40,13 +93,20 @@ public class StreamTest {
     }
     /**
      * Stream过滤并统计
+     * filter 对应Predicate函数式接口
      * @param list
      */
     public static void testLambdaStream (List<Artist> list) {
         long count = list.stream()
-                .filter(artist -> artist.getComeFrom().equals("London"))
+                .filter(new Predicate<Artist>() {
+                    @Override
+                    public boolean test(Artist artist) {
+                        return artist.getComeFrom().equals("London");
+                    }
+                })
                 .count();
         System.out.println(count);
+        //artist -> artist.getComeFrom().equals("London")
     }
 
     /**
